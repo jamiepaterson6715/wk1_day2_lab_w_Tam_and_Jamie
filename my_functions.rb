364b34c73f16e70f1be0221b337bcaffa56c8adc
Tam O'Donnell <thomashodonnell@gmail.com>
2019-04-02 15:19:57 +0100

    first 3 complete

---
+++ my_functions.rb
@@ -0,0 +1,23 @@
+# def greet(name, time_of_day)
+#   return "Good #{time_of_day}, #{name}"
+# end
+#
+# p greet("Gary", "afternoon")
+#
+#
+# def return_10()
+#   return 10
+# end
+
+
+
+#
+# def add(x, y)
+#   return x + y
+# end
+def subtract(a,b)
+  return a - b
+
+end
+
+subtract( 10,5 )
